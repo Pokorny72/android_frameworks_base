017341a607d2e407fcc44e7920126b7ffaed89b1
Albert I <kras@raphielgang.org>
2022-08-23 06:16:52 +0000

    core: pm: Wipe package cache on upgrade
    Squash commits:
    
    Author: Albert I <kras@raphielgang.org>
    Date:   Thu May 12 05:37:43 2022 +0800
    
        core: pm: Wipe package cache when upgrading system
    
        As we keep adding and removing features with each build, it's necessary to have
        package cache wiped out to avoid certain potential issues from arising due to a
        dirty flash.
    
        This commit is meant to supplement existing check for builds with incremental
        version that starts with "eng" (not eng build type, which skips generating
        package cache by design) which will continue to behave as expected.
    
        Signed-off-by: Albert I <kras@raphielgang.org>
    
    Author: Albert I <kras@raphielgang.org>
    Date:   Fri Apr 8 18:59:52 2022 +0800
    
        core: Wipe package cache on upgrade even on user build
    
        As we're spoofing builds by pretending it to be user, let's just do this
        for everyone.
    
        Signed-off-by: Albert I <kras@raphielgang.org>
        Change-Id: I5d967c25bd9c4890549d5d20f5e66b0c6845528c
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Thu Nov 14 01:40:35 2019 +0530
    
        Restore cache clean up code in PackageManagerService
    
        [AnierinBliss]: Forward port to Android 13
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        Signed-off-by: AnierinB <anierin@evolution-x.org>
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ieebe379853b69eb9268814169f58cf7794d8a0a5

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -77,7 +77,7 @@ import java.util.Set;
 
 import javax.crypto.SecretKey;
 
-import com.android.internal.util.tequila.PixelPropsUtils;
+import com.android.internal.util.axiom.PixelPropsUtils;
 
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
